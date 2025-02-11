c17f425e0a5ee54cede699745c2060cd641fa5ea
SrinivasPrasad <43960959+SrinuSandy@users.noreply.github.com>
2025-02-11 21:25:59 +0530

    Take player input and Print the player name on Console window

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-
+    string player_name;
+    cout << "Enter your name: ";
+    cin >> player_name;
+    cout << "Congratulations " << player_name << endl;
     return 0;
 }
