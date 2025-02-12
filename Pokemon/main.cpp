0b9e8435a0d081b3d6b4ce005da7ad5795770e0e
SrinivasPrasad <43960959+SrinuSandy@users.noreply.github.com>
2025-02-12 07:35:19 +0530

    Implement Pokemon selection functionality

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,9 +2,29 @@
 using namespace std;
 
 int main() {
-    string player_name;
-    cout << "Enter your name: ";
-    cin >> player_name;
-    cout << "Congratulations " << player_name << endl;
+    cout << "Welcome to the world of Pokemon! I am professor Oak." << endl;
+    cout << "You can choose one of the following pokemon:" << endl;
+    cout << "1. Bulbasaur" << endl << "2. Charmander" << endl << "3. Squirtle" << endl;
+    cout << "Which pokemon would you like to choose? Enter the number: ";
+    int choice = 0;
+    cin >> choice;
+
+    if (choice == 1) 
+    {
+        cout << "You chose Bulbasaur! A wise choice." << endl;
+    }
+    else if (choice == 2) 
+    {
+        cout << "You chose Charmander! A fiery choice." << endl;
+    }
+    else if (choice == 3) 
+    {
+        cout << "You chose Squirtle! A cool choice." << endl;
+    }
+    else 
+    {
+        cout << "Invalid choice. Please restart the game." << endl;
+    }
+    
     return 0;
 }
