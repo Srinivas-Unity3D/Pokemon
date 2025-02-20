734005a7ff38f07bda891030ac3b267ea7544249
SrinivasPrasad <43960959+SrinuSandy@users.noreply.github.com>
2025-02-20 21:15:08 +0530

    Change the enum to the enum class

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-enum PokemonChoice 
+enum class PokemonChoice 
 {
     Charmander =0, 
     Bulbasaur=1,
@@ -10,8 +10,17 @@ enum PokemonChoice
     InvalidChoice=3
 };
 
+enum class PokemonType
+{
+    Fire, 
+    Electric, 
+    Water, 
+    Earth, 
+    Normal
+};
+
 int main() {
-    PokemonChoice chosen_pokemon = InvalidChoice;
+    PokemonChoice chosen_pokemon = PokemonChoice::InvalidChoice;
     
     string player_name;
     string chosen_pokemon;
@@ -40,28 +49,28 @@ int main() {
 
     switch (choice) {
     case 1:
-        chosen_pokemon = Charmander;
+        chosen_pokemon = PokemonChoice::Charmander;
         cout << "Professor Oak: A fiery choice! Charmander is yours!"<<endl;
         break;
 
     case 2:
-        chosen_pokemon = Bulbasaur;
+        chosen_pokemon = PokemonChoice::Bulbasaur;
         cout << "Professor Oak: A fine choice! Bulbasaur is always ready to grow on you!"<<endl;
         break;
 
     case 3:
-        chosen_pokemon = Squirtle;
+        chosen_pokemon = PokemonChoice::Squirtle;
         cout << "Professor Oak: Splendid! Squirtle will keep you cool under pressure!"<<endl;
         break;
 
     default:
         cout << "Professor Oak: Hmm, that doesn't seem right. Let me choose for you..."<<endl;
-        chosen_pokemon = InvalidChoice; 
+        chosen_pokemon = PokemonChoice::InvalidChoice; 
         cout << "Professor Oak: Just kidding! Let's go with Pikachu, the surprise guest!"<<endl;
         break;
     }
 
-    cout << "Professor Oak: " <<  (chosen_pokemon == Charmander ? "Charmander" : chosen_pokemon == Bulbasaur ? "Bulbasaur" : "Squirtle") << " and you, "
+    cout << "Professor Oak: " <<  (chosen_pokemon == PokemonChoice::Charmander ? "Charmander" : chosen_pokemon == PokemonChoice::Bulbasaur ? "Bulbasaur" : "Squirtle") << " and you, "
         << player_name << ", are going to be the best of friends!"<<endl;
     cout << "Professor Oak: Your journey begins now! Get ready to explore the vast world of Pokemon!"<<endl;
 
